8d7901ec078bd7fb71cca6b85813611e49782aa5
EC2 Default User <ec2-user@ip-172-31-43-220.us-east-2.compute.internal>
2023-01-16 17:17:51 +0000

    fixed formatting of output table

--- scraper.py
+++ scraper.py
@@ -3,14 +3,21 @@
 import requests
 import json
 
+
 #downloading json string from URL which gets automatically updated
 response = json.loads(requests.get("https://prop.kc2g.com/api/stations.json").text)
 
 #print items of interest in the USA only: MUFD, station, time
+print("{:<30} {:<35} {:<20}".format('Station', 'MUFD', 'Time'))
 for item in response:
     mufd = item['mufd']
     name = item['station']['name']
     time = item['time']
     if name.find('USA')>=0:
-        print('Station: ', name, '\t\tMUFD: ', mufd, '\t\tTime: ', time)
-
+#        print('Station: ', name, '\t\tMUFD: ', mufd, '\t\tTime: ', time)
+#        print("Station: %s, MUFD: %s, Time: %s" % (name, mufd, time))
+        print("{:<30} {:<35} {:<20}".format(name, str(mufd), time))
+        
+        
+        
+         
